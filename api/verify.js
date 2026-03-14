19c57044be1e90ec33de662e48c6921dce781f9a
LiveGigs <xrebooking@hotmail.com>
2026-03-15 03:50:57 +0800

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,6 +1,6 @@
-// 不使用 Edge Runtime，使用标准 Node.js
 module.exports = (req, res) => {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.status(200).send('YlllsxOVRq4LBz');
+  res.statusCode = 200;
+  res.end('YlllsxOVRq4LBz');
 };
