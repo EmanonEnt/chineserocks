d1f3c4727d14267baa98c01046f99ae2db8f21c2
LiveGigs <xrebooking@hotmail.com>
2026-03-15 03:55:40 +0800

    Delete api/verify.js

--- api/verify.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.statusCode = 200;
-  res.end('YlllsxOVRq4LBz');
-};
